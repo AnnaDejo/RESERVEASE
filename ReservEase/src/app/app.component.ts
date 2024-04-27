e59ec58c3d1b68a3e3303b6787f7bded41531ab2
AnnaDejo <annadejo12a@gmail.com>
2024-04-27 06:20:17 +0530

    April 27 6:15 am

--- ReservEase/src/app/app.component.ts
+++ ReservEase/src/app/app.component.ts
@@ -7,14 +7,15 @@ import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MyprofileComponent } from './pages/myprofile/myprofile.component';
+import { ResourcesComponent } from "./pages/resources/resources.component";
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,HomeComponent,SigninComponent,SignupComponent,NavComponent,HttpClientModule,MyprofileComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HomeComponent, SigninComponent, SignupComponent, NavComponent, HttpClientModule, MyprofileComponent, ResourcesComponent]
 })
 export class AppComponent {
   title = 'ReservEase';
